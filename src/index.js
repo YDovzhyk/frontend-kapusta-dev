5ae163c93ae5ed7d519d08f4be72101734783a1a
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2023-03-02 18:22:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -15,15 +15,57 @@ import 'styles/styles.scss';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(
-  // <React.StrictMode>
+const [buildVertion, setBuildVertion] = useState(true);
+
+useEffect(() => {
+  if(process.env.NODE_ENV === "production") {
+    setBuildVertion(true)
+  }
+  if(process.env.NODE_ENV === "development") {
+    setBuildVertion(false)
+  }
+  
+}, [process.env.NODE_ENV])
+
+if(buildVertion) {
+  return root.render(
+    // <React.StrictMode>
   <HashRouter>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistedStore}>
-        <ScrollToTop />
-        <App />
-      </PersistGate>
-    </Provider>
-    </HashRouter>
-  // </React.StrictMode>
-);
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistedStore}>
+      <ScrollToTop />
+      <App />
+    </PersistGate>
+  </Provider>
+</HashRouter>
+// </React.StrictMode>
+  );
+} else {
+  return root.render(
+    // <React.StrictMode>
+  <HashRouter>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistedStore}>
+      <ScrollToTop />
+      <App />
+    </PersistGate>
+  </Provider>
+</HashRouter>
+// </React.StrictMode>
+  );
+}
+
+// root.render(
+//     // <React.StrictMode>
+//   <HashRouter>
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistedStore}>
+//       <ScrollToTop />
+//       <App />
+//     </PersistGate>
+//   </Provider>
+// </HashRouter>
+// // </React.StrictMode>
+
+  
+// );
